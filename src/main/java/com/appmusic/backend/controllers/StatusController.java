8a2f02ce30c891caa4a582977d34fb3113e11633
Christiane Sossoukpe <christdebo68@gmail.com>
2024-09-02 17:36:27 +0100

    ajout de dependances d'authentification d'utilisateur.

--- src/main/java/com/appmusic/backend/controllers/StatusController.java
+++ src/main/java/com/appmusic/backend/controllers/StatusController.java
@@ -3,10 +3,15 @@ package com.appmusic.backend.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.appmusic.backend.models.Album;
+import com.appmusic.backend.models.Status;
+import com.appmusic.backend.services.StatusService;
 
 @RestController
 @RequestMapping("/api/users")
@@ -22,8 +27,9 @@ return  statusService.getStatus();
 
 
 @GetMapping("/{id}")
-public Status getStatusById(@PathVariable Long id) { 
-    return statusService.getStatusById(id);
+public ResponseEntity<Status>getStatusById(@PathVariable Long id) { 
+    Status status = statusService.getStatusById(id);
+    return ResponseEntity.ok(status);
 }
     
 }
